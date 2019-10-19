c6239e15b1b43c307988994ce1863c81571be69e
Zeblote <zeblote@gmail.com>
2019-10-19 18:22:29 +0200

    [BR] Set up the unreal linux cross compile toolchain

--- externals/cmakemodules/linux/LinuxCrossToolchain.x86_64-unknown-linux-gnu.cmake
+++ externals/cmakemodules/linux/LinuxCrossToolchain.x86_64-unknown-linux-gnu.cmake
@@ -3,7 +3,7 @@ IF(NOT $ENV{PM_PACKAGES_ROOT} EQUAL "")
 
 	INCLUDE(CMakeForceCompiler)
 
-	SET(LINUX_ROOT $ENV{PM_CLANGCROSSCOMPILE_PATH}/x86_64-unknown-linux-gnu)
+	SET(LINUX_ROOT "$ENV{LINUX_MULTIARCH_ROOT}/../v17_clang-10.0.1-centos7/x86_64-unknown-linux-gnu")
 	STRING(REGEX REPLACE "\\\\" "/" LINUX_ROOT ${LINUX_ROOT})
 
 	message (STATUS "LINUX_ROOT is '${LINUX_ROOT}'")
@@ -36,6 +36,13 @@ IF(NOT $ENV{PM_PACKAGES_ROOT} EQUAL "")
 	#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 	
 	SET(CMAKE_MAKE_PROGRAM "$ENV{PM_MINGW_PATH}/bin/mingw32-make.exe")
+
+	# unreal custom libc++ stuff
+	SET(CMAKE_CXX_FLAGS "-I $ENV{BRICKADIA_UNREAL_DIR}Engine/Source/ThirdParty/Linux/LibCxx/include -I $ENV{BRICKADIA_UNREAL_DIR}Engine/Source/ThirdParty/Linux/LibCxx/include/c++/v1")
+	SET(UE_LINKER_FLAGS "-stdlib=libc++ -nodefaultlibs -Wl,--build-id -L $ENV{BRICKADIA_UNREAL_DIR}Engine/Source/ThirdParty/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/ $ENV{BRICKADIA_UNREAL_DIR}Engine/Source/ThirdParty/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/libc++.a $ENV{BRICKADIA_UNREAL_DIR}Engine/Source/ThirdParty/Linux/LibCxx/lib/Linux/x86_64-unknown-linux-gnu/libc++abi.a -lm -lc -lgcc_s")
+	SET(CMAKE_EXE_LINKER_FLAGS ${UE_LINKER_FLAGS})
+	SET(CAMKE_MODULE_LINKER_FLAGS ${UE_LINKER_FLAGS})
+	SET(CMAKE_SHARED_LINKER_FLAGS ${UE_LINKER_FLAGS})
 ELSE()
 	MESSAGE("PM_PACKAGES_ROOT  variable not defined!")
 ENDIF()
